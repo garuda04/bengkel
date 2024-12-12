f30b529d5eead5c3a9d1a98cca1e6941ae0b0ff6
wahyu22010 <wahyupratama.purba2004@gmail.com>
2024-12-12 18:24:26 +0700

    .

--- instal.sh
+++ instal.sh
@@ -103,9 +103,9 @@ chmod +x $HOME/Desktop/firefox.desktop
 
 #Set aliases
 echo "
-alias bengkel='proot-distro login bengkel --user $username --shared-tmp'
-#alias zrun='proot-distro login bengkel --user $username --shared-tmp -- env DISPLAY=:1.0 MESA_LOADER_DRIVER_OVERRIDE=zink TU_DEBUG=noconform '
-#alias zrunhud='proot-distro login bengkel --user $username --shared-tmp -- env DISPLAY=:1.0 MESA_LOADER_DRIVER_OVERRIDE=zink TU_DEBUG=noconform GALLIUM_HUD=fps '
+alias debian='proot-distro login debian --user $username --shared-tmp'
+#alias zrun='proot-distro login debian --user $username --shared-tmp -- env DISPLAY=:1.0 MESA_LOADER_DRIVER_OVERRIDE=zink TU_DEBUG=noconform '
+#alias zrunhud='proot-distro login debian --user $username --shared-tmp -- env DISPLAY=:1.0 MESA_LOADER_DRIVER_OVERRIDE=zink TU_DEBUG=noconform GALLIUM_HUD=fps '
 alias hud='GALLIUM_HUD=fps '
 alias ls='eza -lF --icons'
 alias cat='bat '
